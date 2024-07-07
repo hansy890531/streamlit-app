84e304dce197414b5f5b1c738450a994bd802a57
HanQ <127290653+hansy890531@users.noreply.github.com>
2024-07-07 16:34:38 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,5 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-# Telegram WebApp 초기화 및 사용자 정보 가져오기 스크립트
-telegram_js = """
-<script src="https://telegram.org/js/telegram-web-app.js"></script>
-<script type="text/javascript">
-    Telegram.WebApp.ready();
-
-    // Telegram 사용자 정보 가져오기
-    const user = Telegram.WebApp.initDataUnsafe.user;
-    document.addEventListener('DOMContentLoaded', (event) => {
-        if (user) {
-            document.body.insertAdjacentHTML('beforeend', `<p>Telegram 사용자 이름: ${user.first_name} ${user.last_name}</p>`);
-            document.body.insertAdjacentHTML('beforeend', `<p>Telegram 사용자 ID: ${user.id}</p>`);
-            document.body.insertAdjacentHTML('beforeend', `<p>Telegram 사용자 사용자명: ${user.username}</p>`);
-        } else {
-            document.body.insertAdjacentHTML('beforeend', `<p>사용자 정보를 가져올 수 없습니다.</p>`);
-        }
-    });
-</script>
-"""
-
-# Streamlit 마크다운을 사용하여 JavaScript 코드를 포함
-components.html(telegram_js)
-
-st.write("안녕하세요")
+# embed streamlit docs in a streamlit app
+components.iframe("https://hansy890531.github.io/webapp/main.html", height=500)
