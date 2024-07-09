7da4836ac5015e66c16dc5b6611b698aa3e41c76
HanQ <127290653+hansy890531@users.noreply.github.com>
2024-07-09 18:45:32 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,11 +22,17 @@ components.html(html_code)
 
 # JavaScript 코드 작성
 js_code = """
-function getTelegramUser() {
-    var user = Telegram.WebApp.initDataUnsafe.user;
-    return user ? JSON.stringify(user) : 'No user data available';
-}
-getTelegramUser();
+function getUserData() {
+    let tg = window.Telegram.WebApp;
+    if (tg && tg.initDataUnsafe && tg.initDataUnsafe.user) {
+        let userData = {
+            id: tg.initDataUnsafe.user.id,
+            first_name: tg.initDataUnsafe.user.first_name,
+            last_name: tg.initDataUnsafe.user.last_name,
+            username: tg.initDataUnsafe.user.username,
+            language_code: tg.initDataUnsafe.user.language_code
+        };
+    return userData;
 """
 
 # streamlit_js_eval을 사용하여 JavaScript 코드 실행 및 결과 받기
