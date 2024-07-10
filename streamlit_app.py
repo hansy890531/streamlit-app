10e8d9b140a2866137a59755879585a1c036e99c
HanQ <127290653+hansy890531@users.noreply.github.com>
2024-07-10 20:22:27 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,19 +2,59 @@ import streamlit as st
 import streamlit.components.v1 as components
 from streamlit_js_eval import streamlit_js_eval
 
-
 # JavaScript 코드 작성
 js_code = """
 function getUserId() {
     Telegram.WebApp.ready();
     let tg = window.Telegram.WebApp;
     if (tg && tg.initDataUnsafe && tg.initDataUnsafe.user) {
-        console.log(tg.initDataUnsafe.user.id)
-        return tg.initDataUnsafe.user.id;
+        let userId = tg.initDataUnsafe.user.id;
+        postEvent('getUserId', function() {
+            console.log('User ID sent to parent:', userId);
+        }, userId);
+        return userId;
     } else {
         return 'No user data available';
     }
 }
+
+function postEvent(eventType, callback, eventData) {
+    if (!callback) {
+        callback = function () {};
+    }
+    if (eventData === undefined) {
+        eventData = '';
+    }
+    console.log('[Telegram.WebView] > postEvent', eventType, eventData);
+
+    if (window.TelegramWebviewProxy !== undefined) {
+        TelegramWebviewProxy.postEvent(eventType, JSON.stringify(eventData));
+        callback();
+    }
+    else if (window.external && 'notify' in window.external) {
+        window.external.notify(JSON.stringify({eventType: eventType, eventData: eventData}));
+        callback();
+    }
+    else if (isIframe) {
+        try {
+            var trustedTarget = 'https://hannam22.streamlit.app/;
+            // For now we don't restrict target, for testing purposes
+            trustedTarget = '*';
+            if (window.parent !== window.top) {
+                window.parent.parent.postMessage(JSON.stringify({eventType: eventType, eventData: eventData}), trustedTarget);
+            } else {
+                window.parent.postMessage(JSON.stringify({eventType: eventType, eventData: eventData}), trustedTarget);
+            }
+            callback();
+        } catch (e) {
+            callback(e);
+        }
+    }
+    else {
+        callback({notAvailable: true});
+    }
+}
+
 getUserId();
 """
 
