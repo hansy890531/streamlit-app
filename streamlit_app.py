9cd3931380405892af7171709ee2f290217dd769
HanQ <127290653+hansy890531@users.noreply.github.com>
2024-07-10 22:18:26 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 from streamlit_js_eval import streamlit_js_eval
 
-# JavaScript 코드를 문자열로 정의
+# Telegram 웹앱 JavaScript 파일을 head에 삽입하는 코드
 js_code_import = """
 document.head.insertAdjacentHTML("beforeEnd", '<script src="https://telegram.org/js/telegram-web-app.js"></script>');
 """
 
 get_user_data_js_code = """
 const tg = window.Telegram.WebApp;
-if (tg.initDataUnsafe.user) {
+if (tg.initDataUnsafe && tg.initDataUnsafe.user) {
     return tg.initDataUnsafe.user.id;
 } else {
     return "No user data available.";
@@ -24,5 +24,10 @@ st.write("야심찬 신작입니다.")
 import_telegram = streamlit_js_eval(js_expressions=js_code_import, want_output=False, key='import_telegram')
 user_id = streamlit_js_eval(js_expressions=get_user_data_js_code, want_output=True, key='get_user_id')
 
-# 사용자 ID를 화면에 표시
-st.write(f"User ID: {user_id}")
+# user_id 값을 확인하여 적절한 메시지 표시
+if user_id is None:
+    st.write("JavaScript execution returned None.")
+elif user_id == "No user data available.":
+    st.write(user_id)
+else:
+    st.write(f"User ID: {user_id}")
