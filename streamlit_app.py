bea967862358720fc3ad564bd19c583cbdfb233c
HanQ <127290653+hansy890531@users.noreply.github.com>
2024-07-13 19:39:42 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,17 @@
 import streamlit as st
 
+def add_custom_css():
+    css_style = """
+    <style>
+    [data-testid="manage-app-button"] {
+        display: none;
+    }
+    </style>
+    """
+    st.markdown(css_style, unsafe_allow_html=True)
+
+# 커스텀 CSS를 추가합니다.
+add_custom_css()
+
+# 헤더를 추가합니다.
 st.header("한남2구역 Dashboard")
