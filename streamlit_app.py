fee8d958da77f5e9db7c01b1d121cf90387d6da2
HanQ <127290653+hansy890531@users.noreply.github.com>
2024-07-13 19:31:21 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,3 @@
 import streamlit as st
-import streamlit.components.v1 as components
 
-# Declare the custom component
-telegram_web_app = components.declare_component("telegram_web_app", path="./frontend")
-
-# Main Streamlit app
-def main():
-    st.title("Telegram Web App with Streamlit")
-    
-    # Placeholder for the user data
-    user_data = telegram_web_app(user_data=None)
-    
-    # Display user data if available
-    if user_data:
-        st.write("User Data from Telegram Web App:")
-        st.json(user_data)
-    else:
-        st.write("Waiting for user data from Telegram Web App...")
-
-if __name__ == "__main__":
-    main()
+st.header("한남2구역 Dashboard")
